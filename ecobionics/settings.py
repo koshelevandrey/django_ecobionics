155c215a96529c8ab1f0c80fcc8d07a168607044
koshelevandrey <tikr22@gmail.com>
2019-12-08 23:16:45 +0300

    Add django-heroku to requirements

--- ecobionics/settings.py
+++ ecobionics/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -154,3 +155,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
